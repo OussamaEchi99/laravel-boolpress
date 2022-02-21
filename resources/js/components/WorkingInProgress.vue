9748cdd563037aa85c24e94465906a75ec5b2db7
OussamaEchi99 <oussama.echi99@gmail.com>
2022-02-21 16:29:35 +0100

    first commit

---
+++ resources/js/components/WorkingInProgress.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Working in progress...</h1>
+</template>
+
+<script>
+export default {
+    name: 'WorkingInProgress'
+}
+</script>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <WorkInProgress />
+</template>
+
+<script>
+import WorkInProgress from '../components/WorkingInProgress.vue';
+
+export default {
+    name: 'App',
+    components: {
+        WorkInProgress
+    }
+}
+</script>
